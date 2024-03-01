b69bf10cd1567ac79ad1d2d4fca5d9a2a9c68047
Andrew Downing <andrewdowning@Andrews-MacBook-Pro.local>
2024-02-29 21:35:33 -0700

    nvc-nvim-config

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,51 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls"
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    ft = "go",
+    opts = function ()
+    end
+  },
+  {
+    "mfussenegger/nvim-dap",
+    init = function ()
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "dreamsofcode-io/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function (_, opts)
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings("dap_go")
+    end,
+  },
+  {
+    -- Read gopher docs
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function (_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function ()
+      vim.cmd [[silent! GoInstallDeps]]
+    end
+  }
+}
+
+return plugins
